336b6472c1ef62953be695d2fd012fe692f4c087
SnowIceApple <154928907+SnowIceApple@users.noreply.github.com>
2025-06-05 17:27:34 +0900

    add animation

--- js/main.js
+++ js/main.js
@@ -31,19 +31,56 @@ $(document).ready(function(){
 
         var sectionTitBg = sta.querySelector('.section_tit_bg');
 
-        gsap.to(sectionTitBg,{
-            scaleX: 1,
-            duration: 0.6,
-            ease: "power1.inOut",
+        let tl1 = gsap.timeline({
             scrollTrigger: {
                 trigger: sta.querySelector('.section_tit_bg'),
                 start: 'left 65%',
-                markers: true,
                 invalidateOnRefresh: true,
                 containerAnimation: horizontalScroll,
+                onEnter: () => sta.classList.add('active'),
             }
         });
+
     });
 
+    var sectionPic = document.querySelectorAll('.section_pic');
+
+    sectionPic.forEach((sPi, i) => {
+
+        gsap.to(sPi, {
+            scrollTrigger: {
+                trigger: sPi,
+                start: '80% right',
+                invalidateOnRefresh: true,
+                containerAnimation: horizontalScroll,
+                onEnter: () => sPi.classList.add('active'),
+            }
+        });
+
+    });
+
+    var graphBox = document.querySelectorAll('.graph_box');
+
+    graphBox.forEach((gpB, i) => {
+
+        gsap.to(gpB, {
+            scrollTrigger: {
+                trigger: gpB,
+                start: '80% right',
+                invalidateOnRefresh: true,
+                markers: true,
+                containerAnimation: horizontalScroll,
+                onEnter: () => gpB.classList.add('active'),
+            }
+        }); 
+    });
+
+    // $('.graph_box ul li').each(function(){
+    //     var idx = $(this).index();
+    //     $(this).children('.graph_line').css({
+    //         'transition-delay': 0.2 * idx + 's',
+    //     }
+    //     );
+    // });
 
 });
