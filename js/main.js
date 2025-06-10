27dda75248ac92429cbd6dedbb9f36d96217a4be
SnowIceApple <154928907+SnowIceApple@users.noreply.github.com>
2025-06-11 02:04:19 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ sections.forEach((section, i) => {
     ScrollTrigger.create({
         trigger: section,
         start: - vh(20) + 'left',
-        end: () => "+=" + (section.offsetWidth - vh(0.9999)),
+        end: () => "+=" + (section.offsetWidth - vh(0.001)),
         containerAnimation: horizontalScroll,
         invalidateOnRefresh: true,
         id: `section-${i+1}`,
