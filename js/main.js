1173e237d3ec50ed90068e36dc7139d39aa544d2
SnowIceApple <154928907+SnowIceApple@users.noreply.github.com>
2025-06-12 01:06:37 +0900

    add matchmedia

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
+
+    const vh = (coef) => window.innerHeight * (coef/100);
+    const vw = (coef) => window.innerWidth * (coef/100);
+
+    var sectionTitArea = document.querySelectorAll('.section_tit_area');
+    var sectionPic = document.querySelectorAll('.section_pic');
+    var graphBox = document.querySelectorAll('.graph_box');
+    var infoBox = document.querySelectorAll('.infoBox');
+    var topicMainTit = document.querySelectorAll('.topic_main_tit');
+    var sections = gsap.utils.toArray('section');
+
+
+    
     gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 
-
-    var horizontalScroll = gsap.to("#content_box", {
+    ScrollTrigger.matchMedia({
+        "(min-width: 912px)": function(){
+        var horizontalScroll = gsap.to("#content_box", {
         x: () => -(document.querySelector('#content_box').scrollWidth - window.innerWidth),
         ease: "none",
         scrollTrigger: {
@@ -13,29 +27,16 @@ $(document).ready(function(){
             pin: true, 
             anticipatePin: 1,
             // markers: true,
-            invalidateOnRefresh: true,
         }
     });
 
-    $('.header, #nav ul li').on('mouseenter focusin', function(){
-        $('#header').addClass('active');
-    });
-
-    $('.header').on('mouseleave focusout', function(){
-        $('#header').removeClass('active');
-    });
-    
-    var sectionTitArea = document.querySelectorAll('.section_tit_area');
 
     sectionTitArea.forEach((sta, i) => {
-
-        var sectionTitBg = sta.querySelector('.section_tit_bg');
 
         let tl1 = gsap.timeline({
             scrollTrigger: {
                 trigger: sta.querySelector('.section_tit_bg'),
                 start: 'left 70%',
-                invalidateOnRefresh: true,
                 containerAnimation: horizontalScroll,
                 onEnter: () => sta.classList.add('active'),
             }
@@ -43,7 +44,6 @@ $(document).ready(function(){
 
     });
 
-    var sectionPic = document.querySelectorAll('.section_pic');
 
     sectionPic.forEach((sPi, i) => {
 
@@ -51,7 +51,6 @@ $(document).ready(function(){
             scrollTrigger: {
                 trigger: sPi,
                 start: '80% right',
-                invalidateOnRefresh: true,
                 containerAnimation: horizontalScroll,
                 onEnter: () => sPi.classList.add('active'),
             }
@@ -59,7 +58,6 @@ $(document).ready(function(){
 
     });
 
-    var graphBox = document.querySelectorAll('.graph_box');
 
     graphBox.forEach((gpB, i) => {
 
@@ -67,7 +65,6 @@ $(document).ready(function(){
             scrollTrigger: {
                 trigger: gpB,
                 start: '80% right',
-                invalidateOnRefresh: true,
                 containerAnimation: horizontalScroll,
                 onEnter: () => gpB.classList.add('active'),
             }
@@ -82,14 +79,12 @@ $(document).ready(function(){
     //     );
     // });
 
-    var infoBox = document.querySelectorAll('.infoBox');
 
     infoBox.forEach((ifB, i) => {
         gsap.to(ifB, {
             scrollTrigger: {
                 trigger: ifB,
                 start: '100% right',
-                invalidateOnRefresh: true,
                 containerAnimation: horizontalScroll,
                 onEnter: () => ifB.classList.add('active'),
             }
@@ -97,10 +92,7 @@ $(document).ready(function(){
 
     });
 
-    const vh = (coef) => window.innerHeight * (coef/100);
-    const vw = (coef) => window.innerWidth * (coef/100);
 
-    var topicMainTit = document.querySelectorAll('.topic_main_tit');
 
     topicMainTit.forEach((tMT, i) => {
         var topicMainTrack = tMT.querySelector('.topic_main_tit_track');
@@ -114,7 +106,6 @@ $(document).ready(function(){
                 trigger: topicMainTrack,
                 start: - vh(20) + 'left',
                 end: () => "+=" + (topicMainTrack.offsetWidth - topicMainMovTxt.scrollWidth),
-                invalidateOnRefresh: true,
                 containerAnimation: horizontalScroll,
                 scrub: 1,
             }
@@ -137,7 +128,6 @@ document.querySelectorAll('#header a, .section_topic_nav a, .footerNav a').forEa
     });
 });
 
-var sections = gsap.utils.toArray('section');
 
 sections.forEach((section, i) => {
     let relatedLink = document.querySelector(`[data-section="${section.id}"]`)
@@ -147,7 +137,6 @@ sections.forEach((section, i) => {
         start: - vh(20) + 'left',
         end: () => "+=" + (section.offsetWidth - vh(0.001)),
         containerAnimation: horizontalScroll,
-        invalidateOnRefresh: true,
         id: `section-${i+1}`,
         onToggle: () => {
             relatedLink.classList.toggle('active')
@@ -155,6 +144,56 @@ sections.forEach((section, i) => {
     });
     
   });
+
+        }, 
+    "(max-width: 911px)": function(){
+
+    sectionTitArea.forEach((sta, i) => {
+        let tl1 = gsap.timeline({
+            scrollTrigger: {
+                trigger: sta.querySelector('.section_tit_bg'),
+                start: '50% bottom',
+                onEnter: () => sta.classList.add('active'),
+            }
+        });
+    });
+
+        sectionPic.forEach((sPi, i) => {
+
+        gsap.to(sPi, {
+            scrollTrigger: {
+                trigger: sPi,
+                start: '50% bottom',
+                onEnter: () => sPi.classList.add('active'),
+            }
+        });
+
+        });
+
+        graphBox.forEach((gpB, i) => {
+
+        gsap.to(gpB, {
+            scrollTrigger: {
+                trigger: gpB,
+                start: '80% bottom',
+                onEnter: () => gpB.classList.add('active'),
+            }
+        }); 
+    });
+    }
+    });
+
+     window.addEventListener("resize", ScrollTrigger.update);
+    
+    $('.header, #nav ul li').on('mouseenter focusin', function(){
+        $('#header').addClass('active');
+    });
+
+    $('.header').on('mouseleave focusout', function(){
+        $('#header').removeClass('active');
+    });
+    
+   
 
 
 
